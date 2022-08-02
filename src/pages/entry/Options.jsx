7ca2939783eac09520c18d5bea3dd447f1c3d951
taohui <htao@thoughtworks.com>
2022-08-02 18:58:26 +0800

    display image for topping options from server

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
 import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
 
 //run as many times when depdendency change
 import { useEffect, useState } from "react";
@@ -8,6 +10,7 @@ import { useEffect, useState } from "react";
 export default function Options({ optionType }) {
   //useState return array of getter and setter
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   //if dependency array if empty, then it will run when the component mount
   //optionType is scoops or toppings
@@ -15,12 +18,13 @@ export default function Options({ optionType }) {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        //TODO: handle error later
-      });
+      .catch((error) => setError(true));
   }, [optionType]);
 
-  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+  if (error) {
+    return <AlertBanner />;
+  }
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => {
     return (
--- src/pages/entry/test/Options.test.jsx
+++ src/pages/entry/test/Options.test.jsx
@@ -10,3 +10,19 @@ test("displays image for each scoop from mock server", async () => {
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("display images for each topping from mock server", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  const altText = toppingImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
